3ae08ac02048795db2ab5c5c3a66db9e439dce83
megha1627 <113565432+megha1627@users.noreply.github.com>
2022-09-15 06:34:01 +0530

    Add files via upload

---
+++ ML Assigment 1 imp.R
@@ -0,0 +1,8 @@
+head(House_Rent_Dataset) to get data of first 5 coloums
+tail(House_Rent_Dataset) to get data of last 5 coloums
+summary(House_Rent_Dataset) to get median,mean, and max values.
+attach(House_Rent_Dataset) to display the variable.
+
+plot(Rent,Size,main = "scatterplot") to represent the scattlerplot by using plot commd.
+
+
