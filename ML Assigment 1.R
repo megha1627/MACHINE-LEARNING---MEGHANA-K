f5e8a8481e8b5c18b9c77079fc79dd2e1193e0be
megha1627 <113565432+megha1627@users.noreply.github.com>
2022-09-15 06:23:32 +0530

    Add files via upload

---
+++ ML Assigment 1.R
@@ -0,0 +1,9 @@
+head(House_Rent_Dataset)
+tail(House_Rent_Dataset)
+summary(House_Rent_Dataset)
+attach(House_Rent_Dataset)
+plot(Rent,Size,mini="Scattlerplot")
+plot(BHK,Rent,main = "scatterplot")
+plot(Rent,Size,main = "scatterplot")
+
+
